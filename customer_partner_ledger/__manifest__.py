6ac55ca4b0f7136e333c2e5bd530b36995566a7d
Jayliste <jaylisteledama94@gmail.com>
2025-03-03 21:12:40 +0300

    [UPDATE] updated manifest

--- customer_partner_ledger/__manifest__.py
+++ customer_partner_ledger/__manifest__.py
@@ -1,20 +1,34 @@
 {
-    'name': 'Customer Partner Ledger Report',
+    'name': 'Customer / Vendor (Partner) Ledger Report',
+    
     'version': '1.0',
+    
     'category': 'Accounting',
-    'summary': 'Generate a detailed customer ledger report',
+    
+    'summary': 'Generate a detailed ledger report for customers and vendors.',
+
+    'description': """
+        This module enables you to generate a detailed ledger report for your customers and vendors, and gives you a closing balance. For instances where you use opening balances, it takes that into account as well when generating the report.
+    """,
+    
     'author': 'SIMI Technologies',
+    
     'website': 'https://simitechnologies.co.ke',
+    
     'depends': ['account'],
+    
     'data': [
         'security/ir.model.access.csv',
 
-        # 'views/customer_ledger_menu.xml',
         'views/customer_ledger_wizard_view.xml',
         
         'reports/customer_ledger_template.xml',
     ],
+
     'installable': True,
-    'application': True,
+
+    'application': False,
+    
     'license': 'LGPL-3',
 }
+
